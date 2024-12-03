aaf7dc491d8df78045b7b67c988e9d127889340d
GoudSunitha <goudsunitha2004@gmail.com>
2024-12-03 21:12:00 +0530

    Add files via upload

---
+++ random.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    int number;
+    Random random = new Random();
+    number = random.nextInt(6);
+    System.out.println("Generated number is : " + number);
+    
+  }
+}
